717417c445293e564114e8c1b8f08fde0db50386
German Machine <engr.austinuche@gmail.com>
2025-03-13 20:55:05 +0100

    restyled the whole hero section

--- Front-end/src/Pages/Home/components/Hero.jsx
+++ Front-end/src/Pages/Home/components/Hero.jsx
@@ -27,54 +27,67 @@ const Hero = () => {
 
   return (
     <>
-      <section className="w-[90%] mx-auto flex flex-wrap pb-[120px]">
-        <div className="flex w-[60%] gap-[2%] flex-wrap relative top-[60px]">
-          <div className="w-[83%] relative flex items-center justify-center gap-[5%] border border-bluecolor rounded-tl-[35px] rounded-br-[35px] py-[15px]">
-            <div className="w-full flex justify-between px-7">
-              <img src={Search} alt="" className="pr-[4px]" />
-              <input
-                className="focus:border-transparent outline-none font-Inter bg-transparent pl-4 w-[60%] text-[14px] flex-wrap"
-                type="text"
-                placeholder="What skills are you looking for?"
-              />
-              <img src={Filter} alt="" />
+      <section className='w-full py-5 lg:py-10'>
+        <div className="w-[95%] lg:w-[80%] mx-auto flex flex-col lg:flex-row">
+          <div className='w-full px-3 lg:flex lg:flex-col lg:justify-between'>
+            <div>
+              <div className='w-full mx-auto flex justify-center items-center flex-nowrap'>
+                <div className="w-[80%] flex justify-between items-center gap-[2%] px-4 lg:px-8 py-2 lg:py-3 border border-bluecolor rounded-tl-[35px] rounded-br-[35px]">
+                  <img src={Search} alt="" className="mr-[2%]" width={20} />
+                  <input
+                    className="focus:border-transparent outline-none font-Inter bg-transparent w-[90%] text-[12px]"
+                    type="text"
+                    placeholder="What skills are you looking for?"
+                  />
+                  <img src={Filter} alt="" width={20} />
+                </div>
+                <a className="inline-block bg-bluecolor text-white text-sm text-center font-Inter lg:w-[15%] py-[10px] px-[20px] lg:py-[15px] lg:px-[px] rounded-tl-[40px] rounded-br-[40px]"
+                  href="#">Search</a>
+              </div>
+
+              <div className="w-full py-6">
+                <h1 className="max-w-[85%] lg:max-w-full font-Inter text-[38px] lg:text-[40px] font-semibold text-bluecolor leading-[48px] lg:leading-[60px]">
+                  Connecting You with the Best <br />
+                  <span className="phrase-animation leading-[48px]">{phrases[currentPhraseIndex]}</span>
+                </h1>
+                <p className="font-Inter text-[16px] text-greycolor py-3">
+                  Find and hire experts across various fields with ease and trust.
+                </p>
+
+              </div>
             </div>
-          </div>
-          <a
-            className="bg-bluecolor text-white font-Inter w-[15%] py-[15px] px-[25px] rounded-tl-[40px] rounded-br-[40px]"
-            href="#"
-          >
-            Search
-          </a>
-        </div>
 
-        <div className="relative top-[120px] w-[63%]">
-          <h1 className="font-Inter text-[48px] font-bold text-bluecolor leading-[60px]">
-            Connecting You with the Best <br />
-            <span className="phrase-animation">{phrases[currentPhraseIndex]}</span>
-          </h1>
-          <p className="pt-[20px] font-Inter text-[15px] text-greycolor">
-            Find and hire experts across various fields with ease and trust.
-          </p>
+            <div className="hidden lg:block">
+              <p className="font-Inter pb-[5px]">Trusted By:</p>
+              <img src={Trust} alt="" />
+            </div>
 
-          <div className="relative top-[120px]">
-            <p className="font-Inter pb-[5px]">Trusted By:</p>
-            <img src={Trust} alt="" />
-          </div>
-        </div>
-
-        <div className="w-[37%] flex flex-wrap relative top-[-25px]">
-          <div className="w-[47%] relative top-[px] left-11">
-            <img src={Person1} alt="" className="pb-[15px] w-[90%]" />
-            <img src={Person2} alt="" className="w-[90%]" />
           </div>
 
-          <div className="w-[47%] relative top-[70px] left-12">
-            <img src={Person3} alt="" className="pb-[15px] w-[90%]" />
-            <img src={Person4} alt="" className="w-[90%]" />
+          <div className='w-full lg:w-[65%] flex flex-col justify-between'>
+            <div className="w-full mx-auto flex justify-center">
+              <div className='flex flex-col gap-6'>
+                <img src={Person1} alt="" className="w-[80%] mx-auto rounded-tl-[40px] rounded-br-[40px]" />
+                <img src={Person2} alt="" className="w-[80%] mx-auto rounded-tl-[40px] rounded-br-[40px]" />
+              </div>
+
+              <div className='flex flex-col gap-6 mt-5'>
+                <img src={Person3} alt="" className="w-[80%] mx-auto rounded-tl-[40px] rounded-br-[40px]" />
+                <img src={Person4} alt="" className="w-[80%] mx-auto rounded-tl-[40px] rounded-br-[40px]" />
+              </div>
+            </div>
+
+            <div className="block lg:hidden">
+              <p className="font-Inter pt-8 pb-5">Trusted By:</p>
+              <img src={Trust} alt="" />
+            </div>
+
           </div>
+
         </div>
       </section>
+
+
       <style jsx>{`
         .phrase-animation {
           display: inline-block;

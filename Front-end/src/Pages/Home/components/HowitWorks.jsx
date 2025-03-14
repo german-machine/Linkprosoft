d444c5d5d1f97317f5c9160b3f937672fd31388a
German Machine <engr.austinuche@gmail.com>
2025-03-14 03:12:31 +0100

    restyled the whole component

--- Front-end/src/Pages/Home/components/HowitWorks.jsx
+++ Front-end/src/Pages/Home/components/HowitWorks.jsx
@@ -3,36 +3,46 @@ import Arrow from "../../../assets/images/Arrow.svg"
 const HowitWorks = () => {
   return (
     <>
-      <section className="w-[85%]  relative left-[70px] pt-[30px] pb-[30px]">
-        <h1 className="text-bluecolor pb-[10px] font-Inter font-bold text-[30px]">How Linkprosoft Works:</h1>
+      <section className="w-full">
+        <div className="w-[95%] lg:w-[80%] mx-auto py-8">
+          <h1 className="text-bluecolor pb-[10px] font-Inter font-bold text-[30px]">How Linkprosoft Works:</h1>
 
-        <div className="w-full flex items-center justify-center flex-nowrap">
+          <div className="w-full flex flex-col lg:flex-row items-left lg:items-center justify-start lg:justify-center flex-nowrap gap-8 font-Inter lg:gap-4">
 
-          <div className="w-full text- flex flex-col items-center gap-1">
-            <div className="border-3 border rounded-full flex items-center justify-center font-Inter font-semibold text-bluecolor text-[30px] w-[80px] h-[80px] border-bluecolor">1</div>
-            <h1 className="text-[25px] font-bold text-greycolor">Post Your Need</h1>
-            <p className="text-[14px] text-greycolor">Describe the service you need.</p>
+            <div className="w-full text-left lg:text-center flex lg:flex-col items-center gap-3 lg:gap-1">
+              <div className="border-3 border rounded-full flex items-center justify-center font-Inter font-semibold text-bluecolor text-[30px] w-auto h-auto px-[25px] py-[10px] border-bluecolor">1</div>
+              <div className="flex flex-col items-start lg:block">
+                <h1 className="text-[20px] font-bold text-greycolor">Post Your Need</h1>
+                <p className="text-[14px] text-greycolor">Describe the service you need.</p>
+              </div>
+            </div>
+
+            <div className="w-[60%] hidden lg:block x-auto">
+              <img src={Arrow} alt="" />
+            </div>
+
+            <div className="w-full text-left lg:text-center flex lg:flex-col lg:items-center gap-3 lg:gap-1">
+              <div className="border-3 border rounded-full flex items-center justify-center font-Inter font-semibold text-bluecolor text-[30px] w-auto h-auto px-[25px] py-[10px] border-bluecolor">2</div>
+              <div className="flex flex-col items-start lg:block">
+                <h1 className="text-[20px] font-bold text-greycolor">Match with Professionals</h1>
+                <p className="text-[14px] text-greycolor">Get matched with the best professionals nearby</p>
+              </div>
+
+            </div>
+
+            <div className="w-[60%] hidden lg:block x-auto">
+              <img src={Arrow} alt="" />
+            </div>
+
+            <div className="w-full font-Inter text-left lg:text-center flex lg:flex-col items-center gap-3 lg:gap-1" >
+              <div className="border-3 border rounded-full flex items-center justify-center font-Inter font-semibold text-bluecolor text-[30px] w-auto h-auto px-[25px] py-[10px] border-bluecolor">3</div>
+              <div className="flex flex-col items-start  lg:block">
+                <h1 className="text-[20px] font-bold text-greycolor">Hire & Get It Done</h1>
+                <p className="text-[14px] text-greycolor">Hire the professionals and get the job done.</p>
+              </div>
+            </div>
           </div>
 
-          <div className="w-full">
-            <img src={Arrow} alt="" />
-          </div>
-
-          <div className="w-full text-center flex flex-col items-center gap-1">
-            <div className="border-3 border rounded-full flex items-center justify-center font-Inter font-semibold text-bluecolor text-[30px] w-[80px] h-[80px] border-bluecolor">2</div>
-            <h1 className="text-[25px] font-bold text-greycolor">Match with Professionals</h1>
-            <p className="text-[14px] text-greycolor">Get matched with the best professionals nearby</p>
-          </div>
-
-          <div className="w-full">
-            <img src={Arrow} alt="" />
-          </div>
-
-          <div className="w-full font-Inter text-center flex flex-col items-center gap-1 leading-[30px]" >
-            <div className="border-3 border rounded-full flex items-center justify-center font-Inter font-semibold text-bluecolor text-[30px] w-[80px] h-[80px] border-bluecolor">3</div>
-            <h1 className="text-[25px] font-bold text-greycolor">Hire & Get It Done</h1>
-            <p className="text-[14px] text-greycolor">Hire the professionals and get the job done.</p>
-          </div>
         </div>
 
       </section>

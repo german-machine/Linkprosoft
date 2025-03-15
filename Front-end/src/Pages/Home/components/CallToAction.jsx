5dd48dbb0100809d3a6254fc6e1f946925bc77db
German Machine <engr.austinuche@gmail.com>
2025-03-15 06:22:21 +0100

    restyled the whole call to action section

--- Front-end/src/Pages/Home/components/CallToAction.jsx
+++ Front-end/src/Pages/Home/components/CallToAction.jsx
@@ -3,18 +3,18 @@
 const CallToAction = () => {
   return (
     <>
-    <section className="w-[100%] text-center  py-[70px] relative mb-7 leading-[50px] mt-8">
-      <hr className="border-hrcolor relative top-[-40px]"/>
-    <h1 className="text-bluecolor font-Inter font-bold text-[30px]">
-      Find and easily hire the best professional for your project.
-    </h1>
+      <section className="w-full border border-t-hrcolor border-b-hrcolor my-8">
+        <div className="w-[95%] w-[80%] mx-auto py-7">
+          <h1 className="text-bluecolor text-center font-Inter font-bold text-[20px] lg:text-[30px] font-Inter font-bold mb-8">
+            Find and easily hire the best professional for your project.
+          </h1>
 
-    <a className=" bg-bluecolor text-white font-Inter w-[25%] py-[10px] px-[25px] rounded-tl-[40px] rounded-br-[40px]" href="#">
-      Join Linkprosoft
-    </a>
-    <hr className="border-hrcolor relative top-[40px]"/>
-    </section>
-    
+          <a className="block bg-bluecolor text-white text-center font-Inter w-[50%] lg:w-[20%] mx-auto py-[10px] px-[25px] rounded-tl-[40px] rounded-br-[40px]" href="#">
+            Join Linkprosoft
+          </a>
+        </div>
+      </section>
+
     </>
   )
 }

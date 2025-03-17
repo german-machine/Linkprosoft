536e02b302eef6382497672fcba4616ad005b625
German Machine <engr.austinuche@gmail.com>
2025-03-17 06:07:55 +0100

    restyled the whole component

--- Front-end/src/Pages/Home/components/Footer.jsx
+++ Front-end/src/Pages/Home/components/Footer.jsx
@@ -2,73 +2,85 @@ import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { LuCopyright } from "react-icons/lu";
 import { FaXTwitter } from "react-icons/fa6";
-import {categories, about, support, resources} from "../../../data/FooterData.js"
+import { categories, about, support, resources, industries } from "../../../data/FooterData.js"
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-    <footer className="w-[100%] bg-bluecolor pb-[160px] relative z-10 overflow-hidden ">
-      <div className="w-[85%] mx-auto flex justify-between relative top-5 gap-5 ">
-        <div className="relative top-[100px] w-[30%]">
-          <h1 className="font-Inter text-white text-[30px] pb-5 font-bold">Linkprosoft.</h1>
-          <p className="pb-2 font-bold text-white font-Inter text-17px">Follow Us</p>
+      <footer className="w-full bg-bluecolor">
+        <div className="w-[95%] lg:w-[80%] mx-auto py-6 lg:flex justify-center">
 
-        <div className="flex gap-3 ">
-           <FaLinkedin className="   text-white text-[25px]" />
-           <FaXTwitter className="   text-white text-[25px]"/>
-            <FaFacebook className="    text-[25px]"/>
+          <div className="w-full lg:w-[40%] mx-auto flex flex-col items-center justify-center gap-2">
+            <h1 className="font-Inter text-white text-[30px] font-bold">Linkprosoft.</h1>
+            <p className="font-bold text-white font-Inter text-17px mb-2">Follow Us</p>
+            <div className="flex gap-3 ">
+              <Link>
+                <FaLinkedin className="text-white text-[25px]" />
+              </Link>
+              <Link>
+                <FaXTwitter className="text-white text-[25px]" />
+              </Link>
+              <Link>
+                <FaFacebook className="text-[25px]" />
+              </Link>
+            </div>
+          </div>
+
+          <div className="w-[85%] lg:w-full mx-auto flex justify-between lg:justify-center gap-5 py-8 lg:py-6">
+            <div className="flex flex-col lg:flex-row gap-5">
+              <div>
+                <h1 className="font-semibold text-white list-disc font-Inter text-[13px] mb-2">Categories</h1>
+                <ul className="font-semibold text-white pl-6 lg:pl-8 list-disc font-Inter text-[12px] flex flex-col gap-2">
+                  {categories.map((category, index) => (
+                    <li key={index}><Link>{category}</Link></li>
+                  ))}
+                </ul>
+              </div>
+
+              <div>
+                <h1 className="font-semibold text-white list-disc font-Inter text-[13px] mb-2">About</h1>
+                <ul className="font-semibold text-white pl-6 lg:pl-8 list-disc font-Inter text-[12px] flex flex-col gap-2">
+                  {about.map((about, index) => (
+                    <li key={index}><Link>{about}</Link></li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="flex flex-col lg:flex-row gap-5">
+              <div>
+                <h1 className="font-semibold text-white list-disc font-Inter text-[13px] mb-2">Industries</h1>
+                <ul className="font-semibold text-white pl-6 lg:pl-8 list-disc font-Inter text-[12px] flex flex-col gap-2">
+                  {support.map((industries, index) => (
+                    <li key={index}><Link>{industries}</Link></li>
+                  ))}
+                </ul>
+              </div>
+
+              <div>
+                <h1 className="font-semibold text-white list-disc font-Inter text-[13px] mb-2">Support</h1>
+                <ul className="font-semibold text-white pl-6 lg:pl-8 list-disc font-Inter text-[12px] flex flex-col gap-2">
+                  {support.map((support, index) => (
+                    <li key={index}><Link>{support}</Link></li>
+                  ))}
+                </ul>
+              </div>
+
+              <div>
+                <h1 className="font-semibold text-white list-disc font-Inter text-[13px] mb-2">Resources</h1>
+                <ul className="font-semibold text-white pl-6 lg:pl-8 list-disc font-Inter text-[12px] flex flex-col gap-2">
+                  {resources.map((resource, index) => (
+                    <li key={index}><Link>{resource}</Link></li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+
         </div>
-        </div>
+      </footer>
 
-
-        <div className="flex w-[60%] relative justify-between top-[50px]">
-          <div>
-            <h1 className="font-semibold text-white list-disc font-Inter text-[13px]">Categories</h1>
-            <ul className="font-semibold text-white pl-4 list-disc font-Inter text-[12px]">
-              {categories.map((category, index) => (
-                <li key={index}><Link>{category}</Link></li>
-              ))}
-              </ul>
-          </div>
-
-          <div>
-            <h1 className="font-semibold text-white list-disc font-Inter text-[13px]">About</h1>
-            <ul className="font-semibold text-white pl-4 list-disc font-Inter text-[12px]">
-             {about.map((about, index) => (
-              <li key={index}><Link>{about}</Link></li>
-             ))}
-              </ul>
-          </div>
-
-          <div>
-            <h1 className="font-semibold text-white list-disc font-Inter text-[13px]">Support</h1>
-            <ul className="font-semibold text-white pl-4 list-disc font-Inter text-[12px]">
-              {support.map((support, index) => (
-                <li key={index}><Link>{support}</Link></li>
-              ))}
-              </ul>
-          </div>
-
-          <div>
-            <h1 className="font-semibold text-white list-disc font-Inter text-[13px]">Resources</h1>
-            <ul className="font-semibold text-white pl-4 list-disc font-Inter text-[12px]">
-              {resources.map((resource, index) => (
-                <li key={index}><Link>{resource}</Link></li>
-              ))}
-              
-              </ul>
-          </div>
-        </div>
-      </div>
-      <hr className="relative top-[130px]"/>
-    <div  className="relative top-[150px] left-[1040px] flex text-center items-center  text-white">
-    <LuCopyright />
-    <h1 className=" text-[14px]  font-Inter pl-1">  2024 Linkprosoft. All Rights Reserved.</h1>
-    
-    </div>
-    </footer>
-    
     </>
   )
 }

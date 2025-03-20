6ca328a549e992bf5153fce53e5f7514e0854d17
German Machine <engr.austinuche@gmail.com>
2025-03-20 19:35:13 +0100

    reduced the width of the login form box, made the LinkedIn and Google buttons to display block on large and extra-large screens

--- Front-end/src/Pages/Login&SignUp/EmployerLogin.jsx
+++ Front-end/src/Pages/Login&SignUp/EmployerLogin.jsx
@@ -12,12 +12,12 @@ const EmployerLogin = () => {
 
       <section className="w-full lg:bg-[#F6F6F6]">
         <div className="w-[85%] lg:w-[80%] mx-auto flex justify-between items-center py-10 lg:py-5 ">
-          <div className='w-[50% w-full xl:w-[30% mx-auto flex items-center hidden lg:block py-5 px-4'>
+          <div className='w-full mx-auto items-center hidden lg:block py-5 px-4'>
             <img src={OurLogo} alt="" className='w-[70%] mx-auto' />
             <p className='w-full opacity-50 text-center mt-3'>Linkprosoft bridging the gap between employers and employees</p>
           </div>
 
-          <div className='w-full mx-auto bg-transparent lg:bg-white rounded-none lg:rounded-lg shadow-none lg:shadow-lg lg:p-3 lg:px-16 lg:py-10'>
+          <div className='w-full lg:w-[100%] xl:w-[70%] mx-auto bg-transparent lg:bg-white rounded-none lg:rounded-lg shadow-none lg:shadow-lg lg:p-3 lg:px-16 lg:py-10'>
             <h2 className='text-[36px] font-Inter font-[800] leading-[40px] mb-3 md:text-center lg:text-left inline-block md:w-full md:mb-3 xl:max-w-[85%]'>Log in, <br /> start <span className='text-[#4093BA]'>advertising</span> and get <span className='text-[#4093BA]'>working</span></h2>
 
             <form action="">
@@ -29,7 +29,7 @@ const EmployerLogin = () => {
 
               <div className="flex items-center gap-2 mt-3">
                 <input type="checkbox" name="" id="" />
-                <p className='font-[400] font-Inter text-[16px] text-black'>Keep me logged in.</p>
+                <p className='font-[400] font-Inter text-[12px] text-black'>Keep me logged in.</p>
               </div>
 
               <div>
@@ -37,13 +37,13 @@ const EmployerLogin = () => {
               </div>
 
               <div className="xl:flex justify-between items-center font-Inter mb-4">
-                <p className=' text-[14px]'>Don't have an account?
-                  <Link className='text-blue-500 underline hover:text-blue-700 text-[16px]'> Sign Up</Link>
+                <p className=' text-[12px]'>Don't have an account?
+                  <Link className='text-blue-500 underline hover:text-blue-700 text-[12px]'> Sign Up</Link>
                 </p>
-                <Link to='/forget password' className='text-blue-500 underline hover:text-blue-700 text-[14px]'>Forgotten Password?</Link>
+                <Link to='/forget password' className='text-blue-500 underline hover:text-blue-700 text-[12px]'>Forgotten Password?</Link>
               </div>
 
-              <div className='w-full font-Inter flex flex-col md:flex-row lg:flex-col xl:flex-row justify-center items-center gap-y-4 lg:gap-y-4 md:gap-[2%] mb-3'>
+              <div className='w-full font-Inter flex flex-col md:flex-row lg:flex-col xl:flex-ro justify-center items-center gap-y-4 lg:gap-y-4 md:gap-[2%] mb-3'>
                 <Link className='bg-[#006FA3] flex justify-center items-center rounded-md py-3 px-1 gap-2 ' style={{ width: "clamp(70%, 90vw, 100%)" }}
                 >
                   <FaLinkedin className='text-white' />

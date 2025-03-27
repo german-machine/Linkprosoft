671355a295bf49fadd5866184fb274106e0b4de4
German Machine <engr.austinuche@gmail.com>
2025-03-27 19:07:17 +0100

    added extra 8 dummy data

--- Front-end/src/data/feedData2.js
+++ Front-end/src/data/feedData2.js
@@ -55,6 +55,54 @@ export const feedData = [
       description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
       profile: profile,
     },
+    {
+      img: img1,
+      title: 'Architect',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img2,
+      title: 'Land-Scaper',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img3,
+      title: 'Architect',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img1,
+      title: 'Plumber',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img2,
+      title: 'Doctor',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img3,
+      title: 'Architect',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img2,
+      title: 'Doctor',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
+    {
+      img: img1,
+      title: 'Architect',
+      description: 'A Competent UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, and Adobe front end development tools like HTML, CSS, and JavaScript',
+      profile: profile,
+    },
     // Add more items as needed
   ]
 

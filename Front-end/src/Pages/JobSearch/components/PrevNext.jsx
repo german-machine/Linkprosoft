43287cad03fe4849a625a4c0b2708a11faf04fb4
German Machine <engr.austinuche@gmail.com>
2025-03-27 19:24:49 +0100

    reduced the with of the text 'showing all [number of data] results'

--- Front-end/src/Pages/JobSearch/components/PrevNext.jsx
+++ Front-end/src/Pages/JobSearch/components/PrevNext.jsx
@@ -8,8 +8,8 @@ const PrevNext = ({ feedDataLength }) => {
    <>
         <section className="w-full">
             <div className="w-[90%] mx-auto py-4 flex flex-col sm:flex-row justify-between sm:items-center">
-                <div className="w-[60%] sm:w-[35%] lg:w-[20%] rounded-full shadow-md inset-4 border px-2 py-1 flex items-center justify-center mb-3 sm:mb-0">
-                    <p className="font-Inter text-base font-bold text-center"> showing all {feedDataLength} results </p>
+                <div className="w-[60%] sm:w-[35%] lg:w-[15%] rounded-full shadow-md inset-4 border px-2 py-1 flex items-center justify-center mb-3 sm:mb-0">
+                    <p className="font-Inter text-sm font-medium text-center"> showing all {feedDataLength} results </p>
                 </div>
                 <div className="flex justify-between items-center gap-[2%] shadow-md inset-4 rounded-full">
                     <div className="w-[30%] rounded-full px-3 py-1 bg-black flex justify-center items-center gap-2">

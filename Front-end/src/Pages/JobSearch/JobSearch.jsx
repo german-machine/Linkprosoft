32bb532c348c1fa1005830f29cab372f205a56e2
German Machine <engr.austinuche@gmail.com>
2025-03-27 09:42:27 +0100

    imported the feed, PrevNext, Newsletter and footer components

--- Front-end/src/Pages/JobSearch/JobSearch.jsx
+++ Front-end/src/Pages/JobSearch/JobSearch.jsx
@@ -3,15 +3,19 @@ import Navbar from "../JobDetails/components/Navbar2"
 import Suggested from "../JobDetails/components/Suggested"
 import Feed from "./components/Feed"
 import PrevNext from "./components/PrevNext"
+import { feedData } from "../../data/feedData2"
+import Newsletter from "../Home/components/Newsletter"
 
 const JobSearch = () => {
+  const feedDataLength = feedData.length
   return (
     <>
     <Navbar/>
     <Suggested/>
     <Feed/>
-    <PrevNext/>
+    <PrevNext feedDataLength={feedDataLength} />
     <Feed/>
+    <Newsletter />
     <Footer/>
     </>
   )

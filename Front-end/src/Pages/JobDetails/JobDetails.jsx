16dccf2fe277ccf2f6f6ef0fc0616c42db5e3335
German Machine <engr.austinuche@gmail.com>
2025-03-28 00:27:33 +0100

    applying correct path to pagination component; passed feedData length as prop to the pagination component

--- Front-end/src/Pages/JobDetails/JobDetails.jsx
+++ Front-end/src/Pages/JobDetails/JobDetails.jsx
@@ -1,18 +1,21 @@
 import Footer from '../Home/components/Footer'
-import PrevNext from '../JobSearch/components/PrevNext'
+import PrevNext from './components/PrevNext'
 import Feed from './components/Feed'
 import Navbar from './components/Navbar2'
 import Suggested from './components/Suggested'
 import Newsletter from '../Home/components/Newsletter'
+import { feedData } from "../../data/feedData2"
+
 
 
 const JobDetails = () => {
+  const feedDataLength = feedData.length;
   return (
     <>
       <Navbar />
       <Suggested />
       <Feed />
-      <PrevNext />
+      <PrevNext feedDataLength={feedDataLength} />
       <Newsletter />
       <Footer />
     </>

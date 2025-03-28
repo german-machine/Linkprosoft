e97e5fb3f225a93e4214d10f1152fbfd4c30c0fb
German Machine <engr.austinuche@gmail.com>
2025-03-28 22:05:44 +0100

    restyled component and displayed bacground color, text color and button color dynamically

--- Front-end/src/Pages/Login&SignUp/components/HeaderBanner.jsx
+++ Front-end/src/Pages/Login&SignUp/components/HeaderBanner.jsx
@@ -4,22 +4,23 @@ import { Link } from 'react-router-dom';
 
 const HeaderBanner = ({ bgColor, logoColor, btnColor, btnTextColor }) => {
     return (
-        <header className={`w-full bg-[${bgColor}] `}>
-            <nav className="w-[90%] mx-auto py-5 flex items-center justify-between">
-                <div className={`text-3xl md:text-4xl lg:text-4xl font-bold text-[${logoColor}] font-Inter cursor-pointer`}>
-                    <Link to='/'>Linkprosoft.</Link>
-                </div>
-
-                <div className='flex items-center justify-end gap-4'>
-                    <div className={`text-center bg-[${btnColor}] px-6 py-3 rounded-md`}>
-                        <Link href="/" className={`text-[${btnTextColor}] font-Inter font-bold text-base `}>Sign In</Link>
+        <header className={`w-full border-b-[3px]`} style={{ backgroundColor: bgColor, border: btnColor }} >
+            <nav className="w-full">
+                <div className="w-[90%] mx-auto py-4 lg:py-3 flex items-center justify-between">
+                    <div className={`text-3xl md:text-4xl lg:text-4xl font-bold font-Inter cursor-pointer`} style={{ color: logoColor}}>
+                        <Link to='/'>Linkprosoft.</Link>
                     </div>
-                    <div className='w-[15%] lg:w-[12%] hidden md:block'>
-                        <img src={profile} alt="" className='rounded-full' />
+
+                    <div className='flex items-center justify-end gap-4'>
+                        <div className={`text-center px-6 py-2 rounded-md`} style={{ backgroundColor: btnColor }}>
+                            <Link href="/" className={`font-Inter font-bold text-base `} style={{ color: btnTextColor }}>Sign In</Link>
+                        </div>
+                        <div className='w-[15%] lg:w-[12%] hidden md:block'>
+                            <img src={profile} alt="" className='rounded-full' />
+                        </div>
                     </div>
                 </div>
             </nav>
-            <hr className={`bg-[${btnColor}] py-[1px]`}/>
         </header>
     )
 }

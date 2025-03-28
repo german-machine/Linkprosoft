abe2c225881725fae9dc1fde86245ac69800c7e9
German Machine <engr.austinuche@gmail.com>
2025-03-28 20:56:05 +0100

    changed the two buttons from button tag to link tags

--- Front-end/src/Pages/Login&SignUp/SignUpAs.jsx
+++ Front-end/src/Pages/Login&SignUp/SignUpAs.jsx
@@ -23,13 +23,13 @@ const SignUpAs = () => {
             <div className='bg-white drop-shadow-lg rounded-lg w-[80%] mx-auto py-4 px-4 mb-6 sm:mb-0'>
               <h2 className="text-2xl font-bold font-Inter mb-3 text-center">Professional</h2>
               <p className='font-Inter text-[16px] pb-8 text-center'>Looking for your next gig? Be a part of Linkprosoft and leverage its opportunities.</p>
-              <button type="button" className="bg-bluecolor font-bold font-Inter text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as Finder</button>
+              <Link className="inline-block text-center bg-bluecolor font-bold font-Inter text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as Finder</Link>
             </div>
 
             <div className='bg-white drop-shadow-lg rounded-lg w-[80%] mx-auto py-4 px-4'>
               <h2 className="text-2xl font-bold font-Inter mb-3 text-center">Employer</h2>
               <p className='font-Inter text-[16px] pb-8 text-center'>Are you an employer or business owner? Advertise your business with Linkprosoft.</p>
-              <button type="button" className="bg-bluecolor font-bold font-Inter text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as Employer</button>
+              <Link className="inline-block text-center bg-bluecolor font-bold font-Inter text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as Employer</Link>
             </div>
 
           </div>

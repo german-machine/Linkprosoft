4be6bf7840b6983dcb842abd24c038398887267a
German Machine <engr.austinuche@gmail.com>
2025-05-08 21:48:47 +0100

    restyled and removed importation of ProfileBody component

--- Front-end/src/Pages/UserDashboard/Routes/Projects.jsx
+++ Front-end/src/Pages/UserDashboard/Routes/Projects.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProjectsBody from '../components/ProjectsBody'
+import { Outlet } from 'react-router-dom'
 
 const Projects = () => {
   return (
-   <>
-   <ProjectsBody/>
-   </>
+    <section className='w-full lg:w-[70%] xl:w-[75%]'>
+      
+      <Outlet />
+    </section>
   )
 }
 

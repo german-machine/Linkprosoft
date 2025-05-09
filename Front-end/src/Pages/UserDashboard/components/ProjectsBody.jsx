a7158ec204bff45ab8631514432234024bd40dc6
German Machine <engr.austinuche@gmail.com>
2025-05-09 12:11:52 +0100

    gave proper html structure for responsiveness

--- Front-end/src/Pages/UserDashboard/components/ProjectsBody.jsx
+++ Front-end/src/Pages/UserDashboard/components/ProjectsBody.jsx
@@ -5,39 +5,35 @@ const ProjectsBody = () => {
   const location = useLocation()
 
   return (
-    <>
-      <section>
+    <section className="w-full mt-5 lg:mt-0 min-h-screen">
+      <div className="w-full lg:w-[90% mx-auto py- px-4 lg:rounded-r-md flex flex-col justify-around">
         <h1 className='font-Inter text-4xl mb-6 font-bold'>Projects</h1>
         <div className='flex gap-14 font-Inter'>
-          <Link 
-            to="pending" 
-            className={`transition-colors duration-300 ${
-              location.pathname.endsWith('pending') || location.pathname === '/UserDashboard/projects' ? 'text-black' : 'text-[#00000099]'
-            }`}
+          <Link
+            to="pending"
+            className={`transition-colors duration-300 ${location.pathname.endsWith('pending') || location.pathname === '/UserDashboard/projects' ? 'text-black' : 'text-[#00000099]'
+              }`}
           >
             Pending
           </Link>
-          <Link 
-            to="completed" 
-            className={`transition-colors duration-300 ${
-              location.pathname.endsWith('completed') ? 'text-black' : 'text-[#00000099]'
-            }`}
+          <Link
+            to="completed"
+            className={`transition-colors duration-300 ${location.pathname.endsWith('completed') ? 'text-black' : 'text-[#00000099]'
+              }`}
           >
             Completed
           </Link>
-          <Link 
-            to="rejected" 
-            className={`transition-colors duration-300 ${
-              location.pathname.endsWith('rejected') ? 'text-black' : 'text-[#00000099]'
-            }`}
+          <Link
+            to="rejected"
+            className={`transition-colors duration-300 ${location.pathname.endsWith('rejected') ? 'text-black' : 'text-[#00000099]'
+              }`}
           >
             Rejected
           </Link>
         </div>
-        <div className='w-full h-[5px] bg-[#F6F6F6]'></div>
-        <Outlet />
-      </section>
-    </>
+      </div>
+      <Outlet />
+    </section>
   )
 }
 

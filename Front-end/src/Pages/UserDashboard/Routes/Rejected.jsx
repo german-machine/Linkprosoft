e6d4148d3f547407a90508267fb290ff68b2901c
German Machine <engr.austinuche@gmail.com>
2025-05-09 12:14:34 +0100

    restyled for both desktop and mobile screens

--- Front-end/src/Pages/UserDashboard/Routes/Rejected.jsx
+++ Front-end/src/Pages/UserDashboard/Routes/Rejected.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { FcAndroidOs } from 'react-icons/fc'
 
 const Rejected = () => {
   return (
-    <div>Rejected</div>
+    <section className="w-full">
+      <div className='w-full lg:w-[90%] mx-auto py-8'>
+        <div className='flex gap-3 bg-[#D9D9D94D] py-2 px-5 mb-4 font-Inter text-sm rounded-md'>
+          <div className='flex items-center'>
+            <FcAndroidOs className='text-[4rem] lg:text-[8rem]' />
+          </div>
+
+          <div className='font-Inter text-sm'>
+            <p className='text-2xl font-bold'>Android</p>
+            <p>A competetnt UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, Illustrator</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 

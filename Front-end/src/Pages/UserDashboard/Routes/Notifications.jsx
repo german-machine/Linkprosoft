80d095cb73af75e6f41726bd916a4c6e79874391
German Machine <engr.austinuche@gmail.com>
2025-05-09 12:13:21 +0100

    reduced the size of the icon

--- Front-end/src/Pages/UserDashboard/Routes/Notifications.jsx
+++ Front-end/src/Pages/UserDashboard/Routes/Notifications.jsx
@@ -7,7 +7,7 @@ const Notifications = () => {
       <div className='w-full lg:w-[90%] mx-auto py-8'>
         <div className='flex gap-3 bg-[#D9D9D94D] py-2 px-5 mb-4 font-Inter text-sm rounded-md'>
           <div className='flex items-center'>
-            <FcGoogle className='text-[4rem] lg:text-[8rem]' />
+            <FcGoogle className='text-[4rem] lg:text-[5rem]' />
           </div>
 
           <div className='font-Inter text-sm'>

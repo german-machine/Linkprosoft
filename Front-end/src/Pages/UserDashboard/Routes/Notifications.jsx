63e7acae7a8a32c68dcf324266f74010282fd683
German Machine <engr.austinuche@gmail.com>
2025-05-09 11:55:15 +0100

    adjusted style to fit mobile screen

--- Front-end/src/Pages/UserDashboard/Routes/Notifications.jsx
+++ Front-end/src/Pages/UserDashboard/Routes/Notifications.jsx
@@ -5,15 +5,15 @@ const Notifications = () => {
   return (
     <section className="w-full">
       <div className='w-full lg:w-[90%] mx-auto py-8'>
-        <div className='flex bg-[#D9D9D94D] py-2 px-5 mb-4 font-Inter text-sm rounded-md'>
-        <div className=''>
-        <FcGoogle className='text-[8rem]' />
-        </div>
+        <div className='flex gap-3 bg-[#D9D9D94D] py-2 px-5 mb-4 font-Inter text-sm rounded-md'>
+          <div className='flex items-center'>
+            <FcGoogle className='text-[4rem] lg:text-[8rem]' />
+          </div>
 
-        <div className='font-Inter text-sm'>
-          <p className='text-2xl font-bold'>Google</p>
-          <p>A competetnt UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, Illustrator and front end toools HTML, CSS , JavaScript</p>
-        </div>
+          <div className='font-Inter text-sm'>
+            <p className='text-2xl font-bold'>Google</p>
+            <p>A competetnt UI designer with visual appealing designs and a work experience of 5 years with knowledge of Figma, Adobe XD, Illustrator</p>
+          </div>
         </div>
       </div>
     </section>

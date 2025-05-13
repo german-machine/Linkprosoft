b82adab9a4d403b56f8347b1b5e8a4222975f9de
German Machine <engr.austinuche@gmail.com>
2025-05-13 03:08:48 +0100

    moved file from Routes folder to components folder

---
+++ Front-end/src/Pages/UserDashboard/components/GigsOptionsBody.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './GigsOptionsBanner'
+import GigsOptionsLinks from './GigsOptionsLinks'
+
+const GigsOptionsBody = () => {
+  return (
+    <section className='min-h-screen'>
+      <Banner />
+      <GigsOptionsLinks />
+    </section>
+  )
+}
+
+export default GigsOptionsBody

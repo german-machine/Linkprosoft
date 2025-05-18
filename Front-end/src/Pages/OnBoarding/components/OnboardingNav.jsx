4043240ac8af9a0deb2368ac391d888cdf21face
German Machine <engr.austinuche@gmail.com>
2025-05-18 20:32:03 +0100

    restyled for both desktop and mobile screen

--- Front-end/src/Pages/OnBoarding/components/OnboardingNav.jsx
+++ Front-end/src/Pages/OnBoarding/components/OnboardingNav.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
-// import search from '../../JobDetails/assets/images/search.png'
-import profile from '../../JobDetails/assets/images/profile.png'
+import { RiMenu5Fill } from "react-icons/ri";
+import PostGigsButton from '../../../constants/PostGigsButton';
+import SignInButton from '../../../constants/SignInButton';
 
 
 const OnboardingNav = () => {
   return (
-   <>
-   <header className='w-[100%] select-none  border-b-[#006FA3] border-b-4'>
-      <nav className='flex align-center w-[90%] justify-between m-auto pt-9 pb-4' >
-        <div className="text-3xl font-bold text-bluecolor font-Inter w-[15%] cursor-pointer"   >
-                      <Link to='/'>Linkprosoft.</Link>
+    <header className="w-full border-b md:border-b-[4px] border-bluecolor">
+      <div className='w-[90%] mx-auto flex items-center justify-between py-5 md:py-4'>
+        <Link to='/' className='text-3xl font-bold text-bluecolor font-Inter uppercase'>
+          Linkprosoft
+        </Link>
+
+        <div className='hidden md:flex gap-5'>
+          <PostGigsButton>
+            Post Gigs
+          </PostGigsButton>
+
+          <SignInButton>
+            Sign In
+          </SignInButton>
         </div>
-  
-     
-  
-        <div className='flex w-[22%] align-center justify-between'>
-          <a href="/" className='bg-[#03AEFF] font-Inter text-white justify py-2 px-5 rounded-md'>Post Gigs</a>
-          <a href="/" className='bg-[#006FA3] font-Inter text-white justify py-2 px-5 rounded-md '>Sign In</a>
-          <div className='w-[16%] h-[40px] flex rounded-full align-center justify-center '>
-          <img src={profile} alt="" className='rounded-full'/>
-          </div>
+
+        <div className='block md:hidden'>
+          <RiMenu5Fill className='text-3xl font-semibold' />
         </div>
-        </nav>
-      </header>
-   </>
+      </div>
+    </header>
   )
 }
 

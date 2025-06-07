c2530e746872f46044aa5f0175f6458651e0673a
German Machine <engr.austinuche@gmail.com>
2025-06-07 11:15:25 +0100

    conditionally rendered the components on the main section

--- Front-end/src/Pages/GigPosting/components/Main.jsx
+++ Front-end/src/Pages/GigPosting/components/Main.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import ChooseTitle from "../Routes/ChooseTitle";
+import SkillsRequired from "../Routes/SkillsRequired";
+import ScopeOfWork from "../Routes/ScopeOfWork";
+import Budget from "../Routes/Budget";
 
 function Main() {
+    const [isActive, setIsActive] = useState('');
     return (
         <div>
-            
+
+            {
+                isActive == ''
+                    ? <ChooseTitle setIsActive={setIsActive} isActive={isActive} />
+                    : isActive == 'skill-required'
+                        ? <SkillsRequired setIsActive={setIsActive} isActive={isActive} />
+                        : isActive == 'scope-of-work'
+                            ? <ScopeOfWork setIsActive={setIsActive} isActive={isActive} />
+                            : isActive == 'budget'
+                                ? <Budget setIsActive={setIsActive} isActive={isActive} />
+                                : 'No such path found'
+            }
+
         </div>
     )
 }

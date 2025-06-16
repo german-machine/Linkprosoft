58a645b0d2b14c02e0d6c111a488850272706f35
German Machine <engr.austinuche@gmail.com>
2025-06-16 20:00:52 +0100

    context provider of loading state to app

---
+++ Front-end/src/contexts/LoadingContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useContext, useState, useRef } from 'react';
+
+const LoadingContext = createContext();
+
+export const useLoading = () => useContext(LoadingContext);
+
+export const LoadingProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const isInitialLoad = useRef(true);
+
+    return (
+        <LoadingContext.Provider value={{ loading, setLoading, isInitialLoad }}>
+            {children}
+        </LoadingContext.Provider>
+    );
+};

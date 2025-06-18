51b16ee30fca622f73fdcd63d3ff697da07f7fe0
German Machine <engr.austinuche@gmail.com>
2025-06-19 00:11:45 +0100

    function to store the value of what the user wants to sign up as

--- Front-end/src/Pages/Login&SignUp/SignUpAs.jsx
+++ Front-end/src/Pages/Login&SignUp/SignUpAs.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import HeaderBanner from './components/HeaderBanner'
+import { useAuth } from '../../contexts/User'
 
 const SignUpAs = () => {
+  const { handleEmployer, handleProfessional } = useAuth();
+  const navigate = useNavigate();
+
+  const handleEmployerAs = () => {
+    handleEmployer('employer');
+    navigate('/register');
+  }
+
+  const handleProfessionalAs = () => {
+    handleProfessional('professional');
+    navigate('/register')
+  }
+
   return (
     <>
       <HeaderBanner
@@ -24,15 +37,15 @@ const SignUpAs = () => {
             <div className='bg-white drop-shadow-lg rounded-lg w-[80%] mx-auto py-4 px-4 mb-6 sm:mb-0'>
               <h2 className="text-2xl font-bold font-Inter mb-3 text-center">Professional</h2>
               <p className='font-Inter text-[16px] pb-8 text-center'>Looking for your next gig? Be a part of Linkprosoft and leverage its opportunities.</p>
-              <Link className="inline-block text-center bg-bluecolor font-bold font-Inter text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as Finder</Link>
+              <button onClick={handleProfessionalAs} className="inline-block text-center bg-bluecolor font-bold font-Inter text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as a Professional</button>
             </div>
 
             <div className='bg-white drop-shadow-lg rounded-lg w-[80%] mx-auto py-4 px-4'>
               <h2 className="text-2xl font-bold font-Inter mb-3 text-center">Employer</h2>
               <p className='font-Inter text-[16px] pb-8 text-center'>Are you an employer or business owner? Advertise your business with Linkprosoft.</p>
-              <Link className="inline-block text-center bg-bluecolor font-bold font-Inter
-                text-white px py-2.5 rounded w-full mb-4 sm:mb-0" to={"/EmployerSignUp"}>Sign Up as Employer
-              </Link>
+              <button onClick={handleEmployerAs} className="inline-block text-center bg-bluecolor font-bold font-Inter
+                text-white px py-2.5 rounded w-full mb-4 sm:mb-0">Sign Up as an Employer
+              </button>
             </div>
 
           </div>

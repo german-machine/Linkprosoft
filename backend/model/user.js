c1c385cf283d215f281d28d2eb2c835429054bb5
German Machine <engr.austinuche@gmail.com>
2025-06-19 00:22:00 +0100

    edited userSchema

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,75 +1,115 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
+    lastName: { type: String, required: true },
+
     email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
+
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 6,
     },
+
     compName: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      trim: true,
+      required: function () {
+        return this.userType === "employer";
+      },
     },
 
-    roles: {
-        type: [String],
-        enum: ["freelancer", "client"],
-        default: ["freelancer"],
-        validate: {
-            validator: function(v) {
-                return v.length <= 2
-            },
-            message: props => "User can have only a maximum of two roles"
-        }
+    userType: {
+      type: String,
+      enum: ["professional", "employer"],
+      required: true,
     },
+  },
+  { timestamps: true }
+);
 
-    activeRole: {
-        type: String,
-        enum: ["freelancer", "client"],
-        required: true
-    },
+const User = mongoose.model("User", userSchema);
+export default User;
 
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-}, {timestamps: true})
+// const userSchema = new mongoose.Schema({
+//     firstName: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//     },
+//     lastName: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//     },
+//     compName: {
+//         type: String,
+//         // required: true,
+//         trim: true,
+//     },
 
+//     // roles: {
+//     //     type: [String],
+//     //     enum: ["professional", "employer"],
+//     //     default: ["professional"],
+//     //     validate: {
+//     //         validator: function(v) {
+//     //             return v.length <= 2
+//     //         },
+//     //         message: props => "User can have only a maximum of two roles"
+//     //     }
+//     // },
 
-userSchema.pre("save", function (next) {
-    if(!this.roles.includes(this.activeRole)){
-        return next(new Error("Active role must be one the assigned roles."))
-    }
-    next()
-})
+//     // activeRole: {
+//     //     type: String,
+//     //     enum: ["professional", "employer"],
+//     //     // required: true
+//     // },
 
-userSchema.methods.switchRole = async function(newRole) {
-    if(!this.roles.includes(newRole)){
-        throw new Error(`User is already using the "${newRole}" role.`)
-    }
-    this.activeRole = newRole
-    await this.save()
-    return this
-}
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// }, {timestamps: true})
 
-const User = mongoose.model("User", userSchema)
+// userSchema.pre("save", function (next) {
+//     // if(!this.roles.includes(this.activeRole)){
+//     //     return next(new Error("Active role must be one the assigned roles."))
+//     // }
+//     next()
+// })
 
-export default User
+// userSchema.methods.switchRole = async function(newRole) {
+//     // if(!this.roles.includes(newRole)){
+//     //     throw new Error(`User is already using the "${newRole}" role.`)
+//     // }
+//     // this.activeRole = newRole
+//     await this.save()
+//     return this
+// }
+
+// const User = mongoose.model("User", userSchema)
+
+// export default User
